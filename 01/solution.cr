cf41a96fe13c4253a1c4a7b227083b834b2c8a6e
Lars Greiving <lgreiving@compeon.de>
2021-12-02 09:35:00 +0100

    Solve day 1.2

--- 01/solution.cr
+++ 01/solution.cr
@@ -1,3 +1,5 @@
 measurements = File.read_lines("./input.txt").map(&.to_i32)
 
 puts measurements.each.cons_pair.count { |a, b| a < b }
+
+puts measurements.each.cons(3).each.cons_pair.count { |a, b| a.sum < b.sum }
